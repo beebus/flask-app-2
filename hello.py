577a11b99cc4c6e184f1fa40773aa08d1556be00
Michael Beebower <michael.beebower@g.austincc.edu>
2020-07-02 00:47:58 -0500

    Updated hello.py with customer error pages. Added more templates and updated the others.

--- hello.py
+++ hello.py
@@ -5,6 +5,16 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -13,7 +23,3 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
-
-
-# if __name__ == '__main__':
-#     app.run()
