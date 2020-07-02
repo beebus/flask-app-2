b568dbcff3c6aa731997dbeeaf24b8e7fe7d4c53
Michael Beebower <michael.beebower@g.austincc.edu>
2020-07-02 00:03:43 -0500

    Updated hello.py with new route /user/<name>

--- hello.py
+++ hello.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
 
 
 if __name__ == '__main__':
